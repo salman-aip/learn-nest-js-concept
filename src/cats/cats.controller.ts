0ba418f4c3b9e08a12587cd6c030910305e4b622
Salman Farshee <salman@aveniripltd.com>
2023-08-21 19:00:17 +0600

    Request-Object for access request data, Wildcard, status-code changes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  @HttpCode(420)
+  create(): string {
+    return 'create new deshi cat';
+  }
+
   @Get()
-  findAll(): string {
-    return 'deshi cats';
+  findAll(@Req() request: Request): string {
+    return `deshi cats ${request}`;
+  }
+
+  @Get(`ab*cd`)
+  findOne(): string {
+    return `WildCard in the middle of the route, supported by only express`;
   }
 }
