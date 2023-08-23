3f8a6257edd2840f3c5a794664caf6dce7959320
Salman Farshee <salman@aveniripltd.com>
2023-08-23 16:53:46 +0600

    define middleware, configure middleware, apply, forRoutes, exclude

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { DogsController } from './dogs/dogs.controller';
 import { AdminController } from './admin/admin.controller';
 import { UsersModule } from './users/users.module';
+import { UserMiddleware } from './users/users.middleware';
+// import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController, CatsController, DogsController, AdminController],
   providers: [AppService],
 })
-export class AppModule {}
+
+//middleware
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UserMiddleware)
+      .exclude({ path: 'cats', method: RequestMethod.ALL }, 'dogs/(.*)')
+      .forRoutes({ path: 'users', method: RequestMethod.GET });
+    // accept string, multiple string, object, controller/multiple controller
+    // .forRoutes(UsersController)
+  }
+}
